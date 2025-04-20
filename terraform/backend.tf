5f8de440133cc4837a83dbcfefcd8737fa24aae0
Sunil <devidehi2022@gmail.com>
2025-04-20 10:44:55 +0530

    modified names

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 # File: terraform/backend.tf
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-12345" # Replace with your S3 bucket name
+    bucket         = "statefilebucket20042025" # Replace with your S3 bucket name
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks" # Optional, for state locking
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,5 +8,5 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "example-bucket-unique-12345" # Ensure this is globally unique
+  default     = "dev4me-20042025" # Ensure this is globally unique
 }
