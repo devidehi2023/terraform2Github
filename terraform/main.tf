fb071e3dcf71e0d7afb545bce806548ea839215f
Sunil <devidehi2022@gmail.com>
2025-04-20 16:10:43 +0530

    modified action

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "example_bucket" {
   bucket = var.bucket_name
 
   tags = {
-    Name        = "Example Bucket"
+    Name        = "Example Bucket1"
     Environment = "Dev"
   }
 }
