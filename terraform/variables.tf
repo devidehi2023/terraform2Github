31ae595436e360d5eba0d3a76b577904594b1e62
Sunil <devidehi2022@gmail.com>
2025-04-20 16:57:41 +0530

    Deploying in West1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,11 +3,11 @@
 variable "aws_region" {
   description = "AWS region for resource deployment"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-1"
 }
 
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "dev4me-20042025" # Ensure this is globally unique
+  default     = "dev4me-West1-20042025" # Ensure this is globally unique
 }
