d299a60a48d270668e2f693b015a81663729b227
Sunil <devidehi2022@gmail.com>
2025-04-20 11:19:40 +0530

    modified backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "statefilebucket20042025" # Replace with your S3 bucket name
     key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    #dynamodb_table = "terraform-locks" # Optional, for state locking
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks" # Optional, for state locking
   }
 }
