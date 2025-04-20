fbe3d2a357e5dd21342f3109346050116b5f94ff
Sunil <devidehi2022@gmail.com>
2025-04-20 16:24:16 +0530

    modified action 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,8 @@ resource "aws_s3_bucket" "example_bucket" {
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.example_bucket.id
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    #object_ownership = "BucketOwnerPreferred"
+    object_ownership = "ObjectWriter"
   }
 }
 
